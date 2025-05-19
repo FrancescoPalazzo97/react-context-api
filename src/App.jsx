8ed4b9a764fac1d52917b8204b4f43eb53ba6943
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-19 14:50:02 +0200

    [UPD] Create context and import it in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import posts from "./assets/posts"
+import PostsContext from "./contexts/PostsContext"
+
 function App() {
 
   return (
-    <>
-
-    </>
+    <PostsContext.Provider value={posts}>
+    </PostsContext.Provider>
   )
 }
 
