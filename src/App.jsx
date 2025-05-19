072475a76dc689e3bf53294c04864db6e50c0676
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-19 15:06:37 +0200

    [UPD] Import PostsContext into PostsPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import posts from "./assets/posts"
 import PostsContext from "./contexts/PostsContext"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./layout/Layout"
+import PostsPage from "./components/PostsPage"
 
 function App() {
 
   return (
     <PostsContext.Provider value={posts}>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={Layout}>
+            <Route path="/">
+              <Route index Component={PostsPage} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </PostsContext.Provider>
   )
 }
---
+++ src/components/PostsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContext } from 'react'
+import PostsContext from '../contexts/PostsContext'
+
+const PostsPage = () => {
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+export default PostsPage
