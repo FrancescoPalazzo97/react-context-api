2aa6c8fed884f072dd5ed27ca101bf93bdda8405
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-19 16:30:50 +0200

    [FEAT] Add alert component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = () => {
+    return (
+        <div className='alert alert-type-success'>
+            Il post è stato creato.
+        </div>
+    )
+}
+
+export default Alert
--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PostsList from './PostsList'
+import Alert from './Alert'
 
 const PostsPage = () => {
     return (
         <main>
             <div className="container">
+                <Alert />
                 <PostsList />
             </div>
         </main>
