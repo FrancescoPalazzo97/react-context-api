85d9898d755d8ab729aeb54d20c3ec036b591ad7
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-19 16:38:03 +0200

    [FEAT] Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div>
+            <button>
+                Aggiungi post
+            </button>
+        </div>
+    )
+}
+
+export default Button
--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PostsList from './PostsList'
 import Alert from './Alert'
+import Button from './Button'
 
 const PostsPage = () => {
     return (
         <main>
             <div className="container">
                 <Alert />
+                <Button />
                 <PostsList />
             </div>
         </main>
